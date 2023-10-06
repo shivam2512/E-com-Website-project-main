edf7eea046ef647ecc5ed0b4dc572e18cacbf587
shivam2512 <shivamshinde786@gmail.com>
2023-10-06 09:22:33 -0700

    E-CommWithAuth User specific

--- src/Context.js
+++ src/Context.js
@@ -1,5 +1,5 @@
-import axios from "axios";
-import React, { useCallback, useEffect, useState } from "react";
+import axios from 'axios';
+import React, { useCallback, useEffect, useState } from 'react';
 
 const Context = React.createContext({
   Cart: false,
@@ -15,20 +15,20 @@ const Context = React.createContext({
   logIn: () => {},
   logOut: () => {},
   token: null,
-  userEmail: "",
+  userEmail: '',
+  addItemToCart: () => {},
+  getCartItems: () => {},
 });
 
 export function ContextProvider(props) {
-  //  crud variable                   <==================<<<<
-  const crudUrl = "crud.com Url";
+  const crudUrl = 'https://crudcrud.com/api/c0bdd57e7354493db5d572689620db84'; // Replace with your CRUD API URL
 
-  // Dummy Data
   const List = [
     {
       item: "Stereo Hearts",
       id: "Stereo-Hearts",
-      url: "https://c.saavncdn.com/947/Stereo-Hearts-feat-Adam-Levine--English-2011-20190607045815-500x500.jpg",
-
+      // url: "https://c.saavncdn.com/947/Stereo-Hearts-feat-Adam-Levine--English-2011-20190607045815-500x500.jpg",
+url:"https://iili.io/JdtBR3P.th.jpg",
       price: "150",
       star: 5,
       description:
@@ -37,7 +37,7 @@ export function ContextProvider(props) {
     {
       item: "The Nights",
       id: "The-Nights",
-      url: "https://c.saavncdn.com/184/The-Nights-Avicii-By-Avicii-English-2015-500x500.jpg",
+      url: "https://iili.io/JdtBDPV.th.jpg" ,
       price: "178",
       star: 4,
       description:
@@ -46,7 +46,7 @@ export function ContextProvider(props) {
     {
       item: "Rise",
       id: "Rise",
-      url: "https://c.saavncdn.com/750/Rise-English-2018-20180523230337-500x500.jpg",
+      url: "https://iili.io/JdtCdNa.th.jpg",
       price: "128",
       star: 3,
       description:
@@ -55,7 +55,7 @@ export function ContextProvider(props) {
     {
       item: "Me, Myself & I",
       id: "Me,-Myself-&-I",
-      url: "https://c.saavncdn.com/473/When-It-s-Dark-Out-English-2015-500x500.jpg",
+      url: "https://iili.io/JdtCfPp.th.jpg",
       price: "195",
       star: 5,
       description:
@@ -67,7 +67,7 @@ export function ContextProvider(props) {
     {
       item: "Love it Cup",
       id: "Love-it-Cup",
-      url: "https://c1.wallpaperflare.com/preview/906/25/252/coffee-drink-cafe-cup.jpg",
+      url: "https://iili.io/JdtnBXj.th.jpg",
       price: "75",
       star: 3,
       description:
@@ -76,7 +76,7 @@ export function ContextProvider(props) {
     {
       item: "Legendary T-shirt",
       id: "Legendary-T-shirt",
-      url: "https://c1.wallpaperflare.com/preview/428/266/566/title-photo-logo-shirt.jpg",
+      url: "https://iili.io/JdtnUv9.th.jpg",
       price: "250",
       star: 3,
       description:
@@ -85,7 +85,7 @@ export function ContextProvider(props) {
     {
       item: "Cool Grocery Bag",
       id: "Cool-Grocery-Bag",
-      url: "https://img.freepik.com/premium-photo/mockup-reusable-grocery-bags-with-print-form-large-black-peas-green-leaf-white-isol_248570-2576.jpg?w=360",
+      url: "https://iili.io/JdtnmyF.th.jpg",
       price: "100",
       star: 5,
       description:
@@ -94,7 +94,7 @@ export function ContextProvider(props) {
     {
       item: "Premium Bottles",
       id: "Premium-Bottles",
-      url: "https://mockuptree.com/wp-content/uploads/edd/2021/01/Free_Bottle_Mockups-e1610718967677.jpg",
+      url: "https://iili.io/JdtoxZG.th.jpg",
       price: "395",
       star: 4,
       description:
@@ -102,49 +102,68 @@ export function ContextProvider(props) {
     },
   ];
 
-  // React Hooks
   const [CartList, setCartList] = useState([]);
   const [Cart, setCart] = useState(false);
   const [isLogIn, setLogIn] = useState(false);
   const [token, setToken] = useState(null);
-  const [userEmail, setEmail] = useState("");
+  const [userEmail, setEmail] = useState('');
 
-  // asyn function to getinformation                   <==================<<<<
-  const getCartFromBackend = useCallback(async (email, id) => {
-    const response = await axios.get(`${crudUrl}/cart${email}/${id}`);
-    setCartList(response.data.cartData);
-  }, []);
+  // Function to add an item to the cart and post it to the backend
+  const addCartToBackend = async (cartData) => {
+    try {
+      const storeData = JSON.parse(localStorage.getItem('token'));
 
-  // async function ADD                    <==================<<<<
-  async function addCartToBackend(cartData) {
-    let storeData = JSON.parse(localStorage.getItem("token"));
-    (async () => {
-      const oldUser = await firstCheck(storeData.email);
-      if (oldUser.length === 0) {
-        const response = await axios.post(`${crudUrl}/cart${storeData.email}`, {
+      if (!storeData.id) {
+        const response = await axios.post(`${crudUrl}/${storeData.email}`, {
           cartData: cartData,
         });
-        if (!storeData.id) {
-          storeData.id = response.data._id;
-          localStorage.setItem("token", JSON.stringify(storeData));
-        }
+
+        storeData.id = response.data._id;
+        localStorage.setItem('token', JSON.stringify(storeData));
       } else {
-        await axios.put(`${crudUrl}/cart${storeData.email}/${storeData.id}`, {
+        await axios.put(`${crudUrl}/${storeData.email}/${storeData.id}`, {
           cartData: cartData,
         });
       }
-    })();
-  }
+    } catch (error) {
+      console.error('Error adding to cart:', error);
+    }
+  };
 
-  // VERY FIRST Check For Old Data                   <==================<<<<
+  // Function to get cart items from the backend
+  const getCart = useCallback(async () => {
+    try {
+      const storeData = JSON.parse(localStorage.getItem('token'));
+
+      if (storeData.id) {
+        const response = await axios.get(`${crudUrl}/${storeData.email}/${storeData.id}`);
+        setCartList(response.data.cartData);
+      }
+    } catch (error) {
+      console.error('Error getting cart:', error);
+    }
+  }, [crudUrl]);
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      const storeData = JSON.parse(localStorage.getItem('token'));
+      setEmail(storeData.email);
+      setToken(storeData.token);
+      setLogIn(true);
+      // Call getCart when a user logs in to retrieve cart items
+      getCart();
+    }
+  }, [getCart]);
+
+  // ... (rest of your code, including logIn, logOut, openCart, closeCart, addCart, removeCart, changeCart functions)
   async function firstCheck(email) {
     try {
-      const newResponse = await axios.get(`${crudUrl}/cart${email}`);
+      const newResponse = await axios.get(`${crudUrl}/cart/${email}`);
       if (newResponse.data.length !== 0) {
-        const storeData = JSON.parse(localStorage.getItem("token"));
+        const storeData = JSON.parse(localStorage.getItem('token'));
         if (!storeData.id) {
           storeData.id = newResponse.data[0]._id;
-          localStorage.setItem("token", JSON.stringify(storeData));
+          localStorage.setItem('token', JSON.stringify(storeData));
         }
         return {
           length: newResponse.data[0].cartData.length,
@@ -154,62 +173,39 @@ export function ContextProvider(props) {
         return { length: 0 };
       }
     } catch (error) {
-      console.log(error);
+      console.error('Error checking cart:', error);
     }
   }
 
-  // seting Login from localstorage (Later USE)                   <==================<<<<
-  // useEffect(() => {
-  //   if (localStorage.getItem("token")) {
-  //     const storeData = JSON.parse(localStorage.getItem("token"));
-  //     setToken(storeData.token);
-  //     setEmail(storeData.longemail);
-  //     (async () => {
-  //       const oldUser = await firstCheck(storeData.email);
-  //       if (oldUser.length > 0) {
-  //         getCartFromBackend(storeData.email, oldUser.cartId);
-  //       }
-  //     })();
-  //     setLogIn(true);
-  //   }
-  // }, [getCartFromBackend]);
-
-  // Logging Functions (Later USE)                   <==================<<<<
   function logIn(id, email) {
-    // localStorage.setItem(
-    //   "token",
-    //   JSON.stringify({
-    //     token: id,
-    //     email: email.replace(/[^A-Za-z0-9]/gi, ""),
-    //     longemail: email,
-    //   })
-    // );
-    // setTimeout(() => {
-    //   localStorage.removeItem("token");
-    // }, 1000 * 60 * 5);
-    // const storeData = JSON.parse(localStorage.getItem("token"));
-    // (async () => {
-    //   const oldUser = await firstCheck(storeData.email);
-    //   if (oldUser.length > 0) {
-    //     getCartFromBackend(storeData.email, oldUser.cartId);
-    //   }
-    // })();
-    // setEmail(storeData.longemail);
+    localStorage.setItem(
+      'token',
+      JSON.stringify({
+        token: id,
+        email: email.replace(/[^A-Za-z0-9]/gi, ''),
+      })
+    );
+  
     setEmail(email);
     setToken(id);
     setLogIn(true);
+  
+    // Only set CartList if it's empty, to prevent clearing the cart on login
+    if (CartList.length === 0) {
+      // Call getCart when a user logs in to retrieve cart items
+      getCart();
+    }
   }
+  
 
-  // Log OUT fUCTION
   function logOut() {
-    // localStorage.removeItem("token");                   <==================<<<<
-    setEmail("");
+    localStorage.removeItem('token');
+    setEmail('');
     setToken(null);
     setLogIn(false);
     setCartList([]);
   }
 
-  // Cart Function
   function openCart() {
     setCart(true);
   }
@@ -225,7 +221,7 @@ export function ContextProvider(props) {
       })
     ) {
       data.quantity = 1;
-      // addCartToBackend([...CartList, data]);                   <==================<<<<
+      addCartToBackend([...CartList, data]);
       setCartList((oldCart) => {
         return [...oldCart, data];
       });
@@ -233,11 +229,11 @@ export function ContextProvider(props) {
   }
 
   function removeCart(id) {
-    // addCartToBackend(                   <==================<<<<
-    //   CartList.filter((item) => {
-    //     return item.id !== id;
-    //   })
-    // );
+    addCartToBackend(
+      CartList.filter((item) => {
+        return item.id !== id;
+      })
+    );
     setCartList((oldCart) => {
       const cartData = oldCart.filter((item) => {
         return item.id !== id;
@@ -256,8 +252,38 @@ export function ContextProvider(props) {
       }
       return item;
     });
-    // addCartToBackend(newCartData);                   <==================<<<<
+    addCartToBackend(newCartData);
     setCartList(newCartData);
+  }
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      const storeData = JSON.parse(localStorage.getItem('token'));
+      setEmail(storeData.email);
+      setToken(storeData.token);
+      setLogIn(true);
+      getCart();
+    }
+  }, [getCart]);
+  async function addItemToCart(item) {
+    // Ensure the user is logged in before adding items to the cart
+    if (!isLogIn) {
+      console.error('User is not logged in.');
+      return;
+    }
+
+    try {
+      // Create a copy of the current cart items with the new item
+      const updatedCart = [...CartList, item];
+
+      // Update the cart items in the state
+      setCartList(updatedCart);
+
+      // Update the cart items in the backend
+      await addCartToBackend(updatedCart);
+    } catch (error) {
+      console.error('Error adding item to cart:', error);
+    }
   }
 
   return (
@@ -277,6 +303,8 @@ export function ContextProvider(props) {
         logIn: logIn,
         logOut: logOut,
         token: token,
+        addItemToCart: addItemToCart,
+        getCartItems: getCart,
       }}
     >
       {props.children}
